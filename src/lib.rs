6dff03ebebd366e614ce46b8c06dc0384e264fb8
James Waples <james@wapl.es>
2024-01-15 10:06:07 +0000

    Test cases against paper linked in readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use oars::{
+    constructors::BoseChecked,
+    oa::{OAConstructor, OAResult},
+};
+
+pub fn create(parameter_count: usize, variations_per_param: usize) -> OAResult<usize> {
+    let config = BoseChecked {
+        prime_base: parameter_count,
+        dimensions: variations_per_param,
+    };
+
+    config.verify()?.gen()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use oars::{
+        constructors::{Bose, BoseChecked},
+        oa::OAConstructor,
+    };
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn p2_l2() {
+        let table = create(2, 2).expect("Gen");
+
+        dbg!(table);
+    }
+
+    #[test]
+    fn p3_l2() {
+        let table = create(2, 3).expect("Gen");
+
+        dbg!(table);
+    }
+
+    #[test]
+    fn p3_l3() {
+        let table = create(3, 3).expect("Gen");
+
+        dbg!(table);
+    }
+
+    #[test]
+    fn p2_l3() {
+        let table = create(3, 2).expect("Gen");
+
+        dbg!(table);
     }
 }
